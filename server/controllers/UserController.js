e9d04ff2945b046a452843aefaf859759ea43758
PoliJava <simo.java47@gmail.com>
2025-09-26 12:28:58 +0200

    09261 - Aggiunta modifica password utente.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,9 +1,27 @@
 import { User } from "../models/DietiRealEstatesDB.js";
+import { createHash } from "crypto"; 
 
 export class UserController {
     static async getAllUsers() {
         return User.findAll({
             attributes: { exclude: ['password'] }
         })
+    }
+
+    static async changePassword(req){
+        console.log(req);
+        const userId = req.userId;
+        const user = await User.findByPk(userId);
+        if(!user){
+            throw new Error("User Not Found.");
+        }
+        const oldPasswordHash = createHash("sha256").update(req.body.oldPassword).digest("hex");
+        if(oldPasswordHash !== user.password){
+            throw new Error("Old Password is incorrect.");
+        };
+        user.password = req.body.newPassword;
+        await user.save();
+
+        return {message : "Password changed successfully."};
     }
 }
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { UserController } from "../controllers/UserController.js";
+import { enforceAuthentication } from "../middleware/authorization.js";
 export const userRouter = express.Router();
 
 userRouter.get("/", (req, res) => {
@@ -11,3 +12,16 @@ userRouter.get("/", (req, res) => {
             res.status(500).json({ message: err.message });
         });
 })
+
+userRouter.put("/changepassword", 
+    enforceAuthentication,
+    (req, res) => {
+        UserController.changePassword(req)
+            .then((message) => {
+                res.status(200).json(message);
+            })
+            .catch((err) => {
+                res.status(500).json({ message: err.message });
+            });
+
+    })
