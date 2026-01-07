3b5f71666a947ea540aac045605276352f79d9e3
PoliJava <simo.java47@gmail.com>
2026-01-07 17:57:46 +0100

    010727 - Vecchi cambiamenti

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,62 +4,50 @@ import { ChangePasswordDTO } from "../DTOs/UserDTO.js";
 import { User, Admin } from "../models/DietiRealEstatesDB.js";
 
 export class UserController {
+  static async changePassword(req, res, next) {
+    try {
+      const dto = new ChangePasswordDTO(req.body);
 
-    static async changePassword(req, res, next) {
-        try {
-            const dto = new ChangePasswordDTO(req.body);
-
-        
-            const result = await UserService.changePassword(
-                User,
-                dto,
-                req.userId
-            ); 
-            res.status(200).json(result);
-        }   catch (err) {
-                next(err);
-        }
+      const result = await UserService.changePassword(User, dto, req.userId);
+      res.status(200).json(result);
+    } catch (err) {
+      next(err);
     }
+  }
 
-    static async getUserById(req, res, next){
-        try{
-            const user = await UserService.getUserById(User, req.params.idUser);
+  static async getUserById(req, res, next) {
+    try {
+      const user = await UserService.getUserById(User, req.params.idUser);
 
-            
-            if (!user) {
-                 return res.status(404).json({ message: "User not found" });
-                }
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
 
-            const result = UserMapper.toUserDTO(user);
-            res.status(200).json(result);
-        }
-
-        catch (err) { 
-            next(err);
-        }
+      const result = UserMapper.toUserDTO(user);
+      res.status(200).json(result);
+    } catch (err) {
+      next(err);
     }
+  }
 
-    static async getAllUsers(req, res, next) {
-        try {
-            const users = await UserService.getAllUsers(User);
-            const result = UserMapper.toUserDTOList(users);
-            res.status(200).json(result);
+  static async getAllUsers(req, res, next) {
+    try {
+      const users = await UserService.getAllUsers(User);
+      const result = UserMapper.toUserDTOList(users);
+      res.status(200).json(result);
+    } catch (err) {
+      next(err);
     }
-        catch (err) {
-            next(err);
-        }
+  }
 
+  static async createAdmin(req, res, next) {
+    try {
+      const dto = req.body;
+      const admin = await UserService.createAdmin(User, Admin, dto);
+      const result = UserMapper.toUserDTO(admin);
+      res.status(201).json(result);
+    } catch (err) {
+      next(err);
     }
-
-
-    static async createAdmin(req, res, next){
-        try {
-            const dto = req.body;
-            const admin = await UserService.createAdmin(User, Admin, dto);
-            const result = UserMapper.toUserDTO(admin);
-            res.status(201).json(result);
-        } catch (err) {
-            next(err);
-        }
-}
+  }
 }
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import { AuthenticationRouter } from './routes/AuthenticationRouter.js';
-import { EstateRouter } from './routes/EstateRouter.js';
-import { AgencyRouter } from './routes/AgencyRouter.js';
-import { UserRouter } from './routes/UserRouter.js';
+import express from "express";
+import cors from "cors";
+import { AuthenticationRouter } from "./routes/AuthenticationRouter.js";
+import { EstateRouter } from "./routes/EstateRouter.js";
+import { AgencyRouter } from "./routes/AgencyRouter.js";
+import { UserRouter } from "./routes/UserRouter.js";
 //import { managerRouter } from './routes/managerRouter.js';
 //import { adminRouter } from './routes/adminRouter.js';
-import { AgentRouter } from './routes/AgentRouter.js';
+import { AgentRouter } from "./routes/AgentRouter.js";
 //app.use(cors());
 
 const app = express();
@@ -22,14 +22,13 @@ app.use("/agency", AgencyRouter);
 app.use("/agents", AgentRouter);
 app.use("/user", UserRouter);
 
-
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-      code: err.status || 500,
-      description: err.message || "An error occurred",
-    });
+  res.status(err.status || 500).json({
+    code: err.status || 500,
+    description: err.message || "An error occurred",
   });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
